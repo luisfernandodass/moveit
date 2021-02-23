21bba4414f1cdaa08b0e711f01d166f4582d13a4
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-02-23 17:20:46 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './componentes/Button'
+
+function App() {
+  return (
+    <div>
+<Button color="red"/>
+<Button color="blue"/>
+<Button color="green"/>
+    </div>
+    
+  );
+}
+
+export default App;
